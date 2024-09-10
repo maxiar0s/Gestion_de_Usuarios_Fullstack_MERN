0f7ce9f68a90bd3e595e879a85982c0d94ac2821
maxiar0s <maxi.aros.osorio@gmail.com>
2024-09-10 15:00:17 -0300

    Empezando api rest

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,4 +5,18 @@ const app = express();
 //config
 
 app.set('port', process.env.PORT || 4000)
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+
+//rutas
+app.get('/', (req, res)=>{
+    res.send('Bienvenido a mi api rest full');
+})
+
 module.exports = app;
+
+
+
