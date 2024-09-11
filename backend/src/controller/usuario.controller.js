9309b056b93d01e98ba654bc3d53a64e70e0e764
maxiar0s <maxi.aros.osorio@gmail.com>
2024-09-10 22:59:04 -0300

    controlador

---
+++ backend/src/controller/usuario.controller.js
@@ -0,0 +1,46 @@
+const usuarioCtrl = {}
+const Usuario = require('../models/usuario')
+
+usuarioCtrl.getUser = async (req, res) =>{
+    const usuarios = await Usuario.find()
+    res.json(usuarios)
+}
+
+//post
+usuarioCtrl.createUser = async (req, res) =>{
+    const {nombre, apellido, edad, telefono, correo} = req.body;
+    const newUser = new Usuario({
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad,
+        telefono: telefono,
+        correo: correo
+    })
+    await newUser.save(); 
+    res.json({message: "Usuario creado"})
+}
+
+usuarioCtrl.getUsuario = async (req, res) =>{
+    const usuario = await Usuario.findById(req.params.id)
+    res.json(usuario)
+}
+
+usuarioCtrl.deleteUser = async (req, res) =>{
+    await Usuario.findByIdAndDelete(req.params.id)
+    res.json({message: "Usuario eliminado"})
+}
+
+//put
+usuarioCtrl.updateUser = async (req, res) =>{
+    const {nombre, apellido, edad, telefono, correo} = req.body;
+    await Usuario.findByIdAndUpdate(req.params.id, {
+        nombre,
+        apellido,
+        edad,
+        telefono,
+        correo
+    })
+    res.json({message: "Usuario actualizado"})
+}
+
+module.exports = usuarioCtrl;
