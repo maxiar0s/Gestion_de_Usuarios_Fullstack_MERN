a13b01e08236152a54f105128ade879b7e6b86f5
maxiar0s <maxi.aros.osorio@gmail.com>
2024-09-15 10:11:12 -0300

    peticion get y delete

--- frontend/src/components/ListaUsuarios.js
+++ frontend/src/components/ListaUsuarios.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./styles.css";
 
 const ListaUsuarios = () => {
-  return <div>Lista Usuarios</div>;
+  const [lista, setLista] = useState([]);
+  useEffect(() => {
+    const getUsuarios = async () => {
+      const res = await axios.get("http://localhost:4000/api/usuarios");
+      setLista(res.data);
+    };
+    getUsuarios();
+  }, [lista]);
+  const deleteUsuario = async (id) => {
+    await axios.delete("http://localhost:4000/api/usuarios/" + id);
+  };
+
+  return (
+    <div className="row">
+      {lista.map((list) => (
+        <div className="col-md-4 p-2">
+          <div className="card">
+            <div className="card-header">
+              <h5 className="text-capitalize">{list.nombre}</h5>
+            </div>
+            <div className="card-body">
+              <p className="text-capitalize">Apellido: {list.apellido}</p>
+              <p>Edad: {list.edad}</p>
+              <p>Telefono: +{list.telefono}</p>
+              <p>Correo: {list.correo}</p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-primary"
+                onClick={() => deleteUsuario(list._id)}
+              >
+                Eliminar
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ListaUsuarios;
