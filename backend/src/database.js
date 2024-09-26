8dc100a86881f8ffcaeb5131feaa407e3f733d14
maxiar0s <maxi.aros.osorio@gmail.com>
2024-09-26 20:20:39 -0300

    deploy3

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // cadena de conexion
 
-const URI = process.env.MONGODB_URI
-            ? process.env.MONGODB_URI
-            : 'mongodb://localhost/dbtest'
-mongoose.connect(URI)
+const URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectado a MongoDB Atlas"))
+  .catch((error) => console.error("Error de conexión:", error));
+
+/*mongoose.connect(URI)
 const connection = mongoose.connection;
 connection.once('open', ()=>{
     console.log('base de datos conectada: ', URI);
-})
+})*/
