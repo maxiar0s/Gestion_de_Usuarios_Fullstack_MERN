7f4cc4ae175c25d211a7b8359f60c63ec32aab08
maxiar0s <maxi.aros.osorio@gmail.com>
2024-09-26 20:35:56 -0300

    deploy4

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 //config
-app.set('port', process.env.PORT || 4000)
+app.set("port", process.env.PORT || 3000);
 
 //middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //rutas
-app.get('/', (req, res)=>{
-    res.send('Bienvenido a mi api rest full');
-})
+app.get("/", (req, res) => {
+  res.send("Bienvenido a mi api rest full");
+});
 
 // ruta api de usuarios
-app.use('/api/usuarios', require('./routes/usuario'))
+app.use("/api/usuarios", require("./routes/usuario"));
 
 module.exports = app;
-
-
-
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const app = require('./app')
-require('./database')
+require("dotenv").config();
+const app = require("./app");
+require("./database");
 
 // logica para ejecutar servidor
 
 async function main() {
-    await app.listen(app.get('port'))
-    console.log('servidor ejecutandose en el puerto: ', app.get('port'));
+  await app.listen(app.get("port"));
+  console.log("servidor ejecutandose en el puerto: ", app.get("port"));
 }
 
 main();
